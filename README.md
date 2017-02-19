316f24dbbf698579d6d06abd9766827bec54768c
Andreas Stenlund <a.stenlund@gmail.com>
2017-02-19 16:51:05 +0100

    Implement multiple methods of closing windows, and provide fine-grained control (via app.config) over which application gets what treatment

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # The Closer
 
-Simple utility that, when executed, closes the process which owns the window at the current mouse cursor position.
+This is a utility that, when executed, closes the window at the current mouse cursor position. Multiple methods of closing a window are supported and this behavior is configurable per application via app.config settings.
+
+## Supported methods
+
+- Keyboard: CTRL-W
+- Keyboard: CTRL-F4
+- Windows Message: WM_DESTROY
+- Windows Message: WM_CLOSE
+- Windows Message: WM_QUIT
--- TheCloser/Program.cs
+++ TheCloser/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace TheCloser
+﻿using System.Configuration;
+using System.Diagnostics;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace TheCloser
 {
     public class Program
     {
+        private static readonly InputSimulator InputSimulator = new InputSimulator();
+
         public static void Main()
         {
-            NativeMethods.GetProcessFromMouseCursorPosition().Kill();
+            var windowHandle = NativeMethods.WindowFromPoint(NativeMethods.GetMouseCursorPosition());
+            NativeMethods.SetForegroundWindow(windowHandle);
+            var process = Process.GetProcessById(NativeMethods.GetProcessIdFromWindowHandle(windowHandle));
+
+            switch (ConfigurationManager.AppSettings[process.ProcessName])
+            {
+                case "CTRL-W":
+                    InputSimulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_W);
+                    break;
+                case "CTRL-F4":
+                    InputSimulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.F4);
+                    break;
+                case "WM_DESTROY":
+                    NativeMethods.PostMessage(windowHandle, NativeMethods.WindowsMessage.WM_DESTROY);
+                    break;
+                case "WM_CLOSE":
+                    NativeMethods.PostMessage(windowHandle, NativeMethods.WindowsMessage.WM_CLOSE);
+                    break;
+                case "WM_QUIT":
+                    NativeMethods.PostMessage(windowHandle, NativeMethods.WindowsMessage.WM_QUIT);
+                    break;
+                default:
+                    NativeMethods.PostMessage(windowHandle, NativeMethods.WindowsMessage.WM_CLOSE);
+                    break;
+            }
         }
     }
 }
