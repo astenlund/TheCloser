8dc978c23e9d3f882399ab885f75419c81953113
Andreas Stenlund <andreas.stenlund@tobiidynavox.com>
2021-04-05 01:11:39 +0200

    Upgrade to .NET 5

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using GregsStack.InputSimulatorStandard;
+using GregsStack.InputSimulatorStandard.Native;
+using Microsoft.Extensions.Configuration;
+
+namespace TheCloser
+{
+    public static class Program
+    {
+        private const string DefaultKillMethod = "CTRL-W";
+
+        private static readonly IConfigurationRoot Config = new ConfigurationBuilder()
+            .SetBasePath(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule!.FileName))
+            .AddJsonFile("appsettings.json", true)
+            .Build();
+
+        private static readonly InputSimulator InputSimulator = new();
+
+        private static readonly Dictionary<string, Action<IntPtr>> KillActions = new()
+        {
+            { "WM_DESTROY", handle => NativeMethods.PostMessage(handle, NativeMethods.WindowNotification.WM_DESTROY) },
+            { "WM_CLOSE", handle => NativeMethods.PostMessage(handle, NativeMethods.WindowNotification.WM_CLOSE) },
+            { "WM_QUIT", handle => NativeMethods.PostMessage(handle, NativeMethods.WindowNotification.WM_QUIT) },
+            { "ESCAPE", handle => TrySendKeyPress(handle, VirtualKeyCode.ESCAPE) },
+            { "ALT-F4", handle => TrySendKeyPress(handle, VirtualKeyCode.F4, VirtualKeyCode.LMENU) },
+            { "CTRL-W", handle => TrySendKeyPress(handle, VirtualKeyCode.VK_W, VirtualKeyCode.CONTROL) },
+            { "CTRL-F4", handle => TrySendKeyPress(handle, VirtualKeyCode.F4, VirtualKeyCode.CONTROL) }
+        };
+
+        public static void Main()
+        {
+            var targetHandle = NativeMethods.WindowFromPoint(NativeMethods.GetMouseCursorPosition());
+            var targetProcess = Process.GetProcessById(NativeMethods.GetProcessIdFromWindowHandle(targetHandle));
+            var killMethod = GetKillMethod(targetProcess);
+            var killAction = GetKillAction(killMethod);
+
+            killAction?.Invoke(targetHandle);
+        }
+
+        private static Action<IntPtr>? GetKillAction(string killMethod) =>
+            KillActions.TryGetValue(killMethod, out var killAction) ? killAction : null;
+
+        private static string GetKillMethod(Process process) =>
+            Config[process.ProcessName]?.ToUpperInvariant() ?? DefaultKillMethod;
+
+        private static void TrySendKeyPress(IntPtr handle, VirtualKeyCode keyCode, VirtualKeyCode modifierKeyCode = default)
+        {
+            if (!TrySetForegroundWindow(handle))
+            {
+                return;
+            }
+
+            if (modifierKeyCode != default)
+            {
+                InputSimulator.Keyboard.ModifiedKeyStroke(modifierKeyCode, keyCode);
+            }
+            else
+            {
+                InputSimulator.Keyboard.KeyPress(keyCode);
+            }
+        }
+
+        private static bool TrySetForegroundWindow(IntPtr windowHandle) =>
+            NativeMethods.GetForegroundWindow() == windowHandle ||
+            NativeMethods.SetForegroundWindow(windowHandle);
+    }
+}
