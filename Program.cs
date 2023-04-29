aefa78ca0eb32ae8ed554de709fe3127a7a98e15
Andreas Stenlund <andreas.stenlund@svk.se>
2023-04-29 14:20:50 +0200

    To file-scoped namespaces

--- Program.cs
+++ Program.cs
@@ -7,82 +7,81 @@ using GregsStack.InputSimulatorStandard;
 using GregsStack.InputSimulatorStandard.Native;
 using Microsoft.Extensions.Configuration;
 
-namespace TheCloser
+namespace TheCloser;
+
+public static class Program
 {
-    public static class Program
+    private const string DefaultKillMethod = "CTRL-W";
+
+    private static readonly IConfigurationRoot Config = new ConfigurationBuilder()
+        .SetBasePath(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule!.FileName))
+        .AddJsonFile("appsettings.json", true)
+        .Build();
+
+    private static readonly InputSimulator InputSimulator = new();
+
+    private static readonly Dictionary<string, Action<IntPtr>> KillActions = new()
     {
-        private const string DefaultKillMethod = "CTRL-W";
+        { "WM_DESTROY", handle => NativeMethods.PostMessage(handle, NativeMethods.WindowNotification.WM_DESTROY) },
+        { "WM_CLOSE", handle => NativeMethods.PostMessage(handle, NativeMethods.WindowNotification.WM_CLOSE) },
+        { "WM_QUIT", handle => NativeMethods.PostMessage(handle, NativeMethods.WindowNotification.WM_QUIT) },
+        { "ESCAPE", handle => TrySendKeyPress(handle, VirtualKeyCode.ESCAPE) },
+        { "ALT-F4", handle => TrySendKeyPress(handle, VirtualKeyCode.F4, VirtualKeyCode.LMENU) },
+        { "CTRL-W", handle => TrySendKeyPress(handle, VirtualKeyCode.VK_W, VirtualKeyCode.CONTROL) },
+        { "CTRL-F4", handle => TrySendKeyPress(handle, VirtualKeyCode.F4, VirtualKeyCode.CONTROL) }
+    };
 
-        private static readonly IConfigurationRoot Config = new ConfigurationBuilder()
-            .SetBasePath(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule!.FileName))
-            .AddJsonFile("appsettings.json", true)
-            .Build();
+    private static readonly string LogPath = Path.Combine(Path.GetTempPath(), "TheCloser.txt");
 
-        private static readonly InputSimulator InputSimulator = new();
+    public static void Main()
+    {
+        var targetHandle = NativeMethods.WindowFromPoint(NativeMethods.GetMouseCursorPosition());
+        var targetProcess = Process.GetProcessById(NativeMethods.GetProcessIdFromWindowHandle(targetHandle));
+        var killMethod = GetKillMethod(targetProcess);
+        var killAction = GetKillAction(killMethod);
 
-        private static readonly Dictionary<string, Action<IntPtr>> KillActions = new()
+        Log($"{targetProcess.ProcessName} -> {killMethod}");
+
+        killAction?.Invoke(targetHandle);
+    }
+
+    private static Action<IntPtr>? GetKillAction(string killMethod) =>
+        KillActions.TryGetValue(killMethod, out var killAction) ? killAction : null;
+
+    private static string GetKillMethod(Process process) =>
+        Config[process.ProcessName]?.ToUpperInvariant() ?? DefaultKillMethod;
+
+    private static void Log(string msg) => File.AppendAllText(LogPath, msg + Environment.NewLine);
+
+    private static void TrySendKeyPress(IntPtr handle, VirtualKeyCode keyCode, VirtualKeyCode modifierKeyCode = default)
+    {
+        var (success, error) = TrySetForegroundWindow(handle);
+
+        if (!success)
         {
-            { "WM_DESTROY", handle => NativeMethods.PostMessage(handle, NativeMethods.WindowNotification.WM_DESTROY) },
-            { "WM_CLOSE", handle => NativeMethods.PostMessage(handle, NativeMethods.WindowNotification.WM_CLOSE) },
-            { "WM_QUIT", handle => NativeMethods.PostMessage(handle, NativeMethods.WindowNotification.WM_QUIT) },
-            { "ESCAPE", handle => TrySendKeyPress(handle, VirtualKeyCode.ESCAPE) },
-            { "ALT-F4", handle => TrySendKeyPress(handle, VirtualKeyCode.F4, VirtualKeyCode.LMENU) },
-            { "CTRL-W", handle => TrySendKeyPress(handle, VirtualKeyCode.VK_W, VirtualKeyCode.CONTROL) },
-            { "CTRL-F4", handle => TrySendKeyPress(handle, VirtualKeyCode.F4, VirtualKeyCode.CONTROL) }
-        };
-
-        private static readonly string LogPath = Path.Combine(Path.GetTempPath(), "TheCloser.txt");
-
-        public static void Main()
-        {
-            var targetHandle = NativeMethods.WindowFromPoint(NativeMethods.GetMouseCursorPosition());
-            var targetProcess = Process.GetProcessById(NativeMethods.GetProcessIdFromWindowHandle(targetHandle));
-            var killMethod = GetKillMethod(targetProcess);
-            var killAction = GetKillAction(killMethod);
-
-            Log($"{targetProcess.ProcessName} -> {killMethod}");
-
-            killAction?.Invoke(targetHandle);
+            Log($"Window activation failed (error code: {error})");
+            return;
         }
 
-        private static Action<IntPtr>? GetKillAction(string killMethod) =>
-            KillActions.TryGetValue(killMethod, out var killAction) ? killAction : null;
-
-        private static string GetKillMethod(Process process) =>
-            Config[process.ProcessName]?.ToUpperInvariant() ?? DefaultKillMethod;
-
-        private static void Log(string msg) => File.AppendAllText(LogPath, msg + Environment.NewLine);
-
-        private static void TrySendKeyPress(IntPtr handle, VirtualKeyCode keyCode, VirtualKeyCode modifierKeyCode = default)
+        if (modifierKeyCode != default)
         {
-            var (success, error) = TrySetForegroundWindow(handle);
-
-            if (!success)
-            {
-                Log($"Window activation failed (error code: {error})");
-                return;
-            }
-
-            if (modifierKeyCode != default)
-            {
-                InputSimulator.Keyboard.ModifiedKeyStroke(modifierKeyCode, keyCode);
-            }
-            else
-            {
-                InputSimulator.Keyboard.KeyPress(keyCode);
-            }
+            InputSimulator.Keyboard.ModifiedKeyStroke(modifierKeyCode, keyCode);
         }
-
-        private static (bool success, int error) TrySetForegroundWindow(IntPtr windowHandle)
+        else
         {
-            var success = NativeMethods.GetForegroundWindow() == windowHandle ||
-                          NativeMethods.SetForegroundWindow(windowHandle);
-
-            var error = success
-                ? 0
-                : Marshal.GetLastWin32Error();
-
-            return (success, error);
+            InputSimulator.Keyboard.KeyPress(keyCode);
         }
+    }
+
+    private static (bool success, int error) TrySetForegroundWindow(IntPtr windowHandle)
+    {
+        var success = NativeMethods.GetForegroundWindow() == windowHandle ||
+                      NativeMethods.SetForegroundWindow(windowHandle);
+
+        var error = success
+            ? 0
+            : Marshal.GetLastWin32Error();
+
+        return (success, error);
     }
 }
