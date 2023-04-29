9cc4f5481df2951f512af1d74cfae5f3eb1ff4f7
Andreas Stenlund <a.stenlund@gmail.com>
2023-04-29 17:03:03 +0200

    Update publish.ps1

--- publish.ps1
+++ publish.ps1
@@ -1,10 +1,10 @@
 dotnet publish --configuration 'Release'
 
-$Source = '.\bin\Release\net5.0\win-x64\publish'
+$Source = '.\bin\Release\net7.0-windows\win-x64\publish'
 $Destination = 'C:\Sync\Bin\TheCloser\'
 
 if (!(Test-Path $Destination)) {
     New-Item $Destination -ItemType Directory -Force
 }
 
-Copy-Item "$Source\*" $Destination -Force -Verbose
+Copy-Item "$Source\*" $Destination -Recurse -Force -Verbose
