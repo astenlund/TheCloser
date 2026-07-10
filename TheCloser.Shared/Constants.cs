5ca8e58a3e00724fc532db0dc2528118f193d627
Andreas Stenlund <a.stenlund@gmail.com>
2026-07-10 22:52:00 +0200

    refactor(shared): rename shared memory map to match contents

--- TheCloser.Shared/Constants.cs
+++ TheCloser.Shared/Constants.cs
@@ -7,7 +7,7 @@ public static class Constants
     public const string GuardMutexName = "TheCloserGuardMutex";
     public const string DaemonMutexName = "TheCloserDaemonMutex";
     public const string DaemonExitEventName = "TheCloserDaemonExitEvent";
-    public const string MemoryMappedFileName = "TheCloserStartupTimestamp";
+    public const string MemoryMappedFileName = "TheCloserSharedState";
     public const long MemoryMappedFileSize = 1024;
     public const string DaemonStartArgument = "--start";
     public const string DaemonStopArgument = "--stop";
