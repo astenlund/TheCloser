d58231a9bd6b9620ab214f521c095f1e7bb3ba42
Andreas Stenlund <a.stenlund@gmail.com>
2026-07-10 12:28:31 +0200

    refactor(app): remove dead native code and trim pasted docs

--- TheCloser/NativeMethods.cs
+++ TheCloser/NativeMethods.cs
@@ -1,7 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Runtime.InteropServices;
-using static System.Runtime.InteropServices.CallingConvention;
 using static System.Runtime.InteropServices.CharSet;
 using static System.Runtime.InteropServices.UnmanagedType;
 
@@ -11,7 +10,6 @@ namespace TheCloser;
 internal static class NativeMethods
 {
     public const int SC_CLOSE = 0xF060;
-    public const int SC_RESTORE = 0xF120;
     public const uint GA_ROOT = 2;
     public const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
     public const uint MOUSEEVENTF_LEFTUP   = 0x0004;
@@ -84,16 +82,7 @@ internal static class NativeMethods
     [DllImport("user32.dll", SetLastError = true)]
     [return: MarshalAs(Bool)]
     public static extern bool SetCursorPos(int X, int Y);
-    
-    [DllImport("user32.dll", CharSet = Auto, CallingConvention = StdCall)]
-    public static extern void mouse_event(
-        uint dwFlags,
-        uint dx,
-        uint dy,
-        uint dwData,
-        UIntPtr dwExtraInfo
-    );
-    
+
     [DllImport("user32.dll", SetLastError = true)]
     public static extern uint SendInput(uint nInputs, [MarshalAs(UnmanagedType.LPArray), In] INPUT[] pInputs, int cbSize);
     
@@ -146,8 +135,6 @@ internal static class NativeMethods
     }
     
     public const uint INPUT_MOUSE = 0;
-    public const uint INPUT_KEYBOARD = 1;
-    public const uint INPUT_HARDWARE = 2;
 
     [DllImport("user32.dll", SetLastError = true)]
     public static extern IntPtr WindowFromPoint(Point p);
@@ -164,64 +151,16 @@ internal static class NativeMethods
     public static extern bool PostMessage(IntPtr hWnd, [MarshalAs(U4)] WindowNotification Msg, IntPtr wParam, IntPtr lParam);
 
     /// <summary>
-    ///     Brings the thread that created the specified window into the foreground and activates the window. Keyboard input is
-    ///     directed to the window, and various visual cues are changed for the user. The system assigns a slightly higher
-    ///     priority to the thread that created the foreground window than it does to other threads.
-    ///     <para>See for https://msdn.microsoft.com/en-us/library/windows/desktop/ms633539%28v=vs.85%29.aspx more information.</para>
+    ///     Activates the window and brings its creating thread to the foreground. The system restricts which processes
+    ///     may take the foreground; when the call is denied, Windows flashes the taskbar button instead.
     /// </summary>
-    /// <param name="hWnd">
-    ///     C++ ( hWnd [in]. Type: HWND )<br />A handle to the window that should be activated and brought to the foreground.
-    /// </param>
-    /// <returns>
-    ///     <c>true</c> or nonzero if the window was brought to the foreground, <c>false</c> or zero If the window was not
-    ///     brought to the foreground.
-    /// </returns>
-    /// <remarks>
-    ///     The system restricts which processes can set the foreground window. A process can set the foreground window only if
-    ///     one of the following conditions is true:
-    ///     <list type="bullet">
-    ///     <listheader>
-    ///         <term>Conditions</term><description></description>
-    ///     </listheader>
-    ///     <item>The process is the foreground process.</item>
-    ///     <item>The process was started by the foreground process.</item>
-    ///     <item>The process received the last input event.</item>
-    ///     <item>There is no foreground process.</item>
-    ///     <item>The process is being debugged.</item>
-    ///     <item>The foreground process is not a Modern Application or the Start Screen.</item>
-    ///     <item>The foreground is not locked (see LockSetForegroundWindow).</item>
-    ///     <item>The foreground lock time-out has expired (see SPI_GETFOREGROUNDLOCKTIMEOUT in SystemParametersInfo).</item>
-    ///     <item>No menus are active.</item>
-    ///     </list>
-    ///     <para>
-    ///     An application cannot force a window to the foreground while the user is working with another window.
-    ///     Instead, Windows flashes the taskbar button of the window to notify the user.
-    ///     </para>
-    ///     <para>
-    ///     A process that can set the foreground window can enable another process to set the foreground window by
-    ///     calling the AllowSetForegroundWindow function. The process specified by dwProcessId loses the ability to set
-    ///     the foreground window the next time the user generates input, unless the input is directed at that process, or
-    ///     the next time a process calls AllowSetForegroundWindow, unless that process is specified.
-    ///     </para>
-    ///     <para>
-    ///     The foreground process can disable calls to SetForegroundWindow by calling the LockSetForegroundWindow
-    ///     function.
-    ///     </para>
-    /// </remarks>
-    // For Windows Mobile, replace user32.dll with coredll.dll
     [return: MarshalAs(Bool)]
     [DllImport("user32.dll", SetLastError = true)]
     public static extern bool SetForegroundWindow(IntPtr hWnd);
 
     /// <summary>
-    ///     Retrieves a handle to the foreground window (the window with which the user is currently working). The system
-    ///     assigns a slightly higher priority to the thread that creates the foreground window than it does to other threads.
-    ///     <para>See https://msdn.microsoft.com/en-us/library/windows/desktop/ms633505%28v=vs.85%29.aspx for more information.</para>
+    ///     Retrieves a handle to the foreground window; can be zero in transient states such as a window losing activation.
     /// </summary>
-    /// <returns>
-    ///     C++ ( Type: Type: HWND )<br /> The return value is a handle to the foreground window. The foreground window
-    ///     can be NULL in certain circumstances, such as when a window is losing activation.
-    /// </returns>
     [DllImport("user32.dll")]
     public static extern IntPtr GetForegroundWindow();
 
@@ -231,22 +170,9 @@ internal static class NativeMethods
         public int X;
         public int Y;
 
-        public POINT(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-
-        public POINT(Point pt) : this(pt.X, pt.Y) { }
-
         public static implicit operator Point(POINT p)
         {
             return new Point(p.X, p.Y);
-        }
-
-        public static implicit operator POINT(Point p)
-        {
-            return new POINT(p.X, p.Y);
         }
     }
     
